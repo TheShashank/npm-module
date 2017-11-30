d7f4600786db2848752b5b46ad1d8a3af89d0afe
SR <shashankr.blr@gmail.com>
2017-11-30 17:27:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 "use strict";
-//import * as pluralize from 'pluralize';
 Object.defineProperty(exports, "__esModule", { value: true });
-/*
-export function getPlural (str: any) {
-    return pluralize.plural(str);
-}
-*/
+
 function hello() {
     console.log("Hello World");
 }
